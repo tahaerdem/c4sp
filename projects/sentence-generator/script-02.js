a038330d85a87401f33f16c6ca49a836118d802d
Taha <tahaerdemozturk@gmail.com>
2021-11-23 19:41:24 -0500

    hi

---
+++ projects/sentence-generator/script-02.js
@@ -0,0 +1,6 @@
+let ranks = ['A','B','C','B','C'];
+let nounsa = ["whale", "dinosaur", "monkey", "noah", "spider"];
+
+for(let i = 0; i < ranks.length; i++){
+    console.log(nounsa[i], ranks[i]);
+}
